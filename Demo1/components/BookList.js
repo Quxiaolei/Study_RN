25372e8a488416b05b1c445b9c2821015247ab66
xiaolei <coder_xiaolei@163.com>
2016-07-24 23:27:03 +0800

    ListView OK

--- Demo1/components/BookList.js
+++ Demo1/components/BookList.js
@@ -7,52 +7,95 @@ import {
   View,
   Text,
   Image,
+  ListView,
+  TouchableHighlight,
 } from 'react-native';
 
 let FAKE_BOOK_DATA = [
+    {volumeInfo: {title: 'The Catcher in the Rye', authors: "J. D. Salinger", imageLinks: {thumbnail: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2721385247,1356253863&fm=206&gp=0.jpg'}}},
     {volumeInfo: {title: 'The Catcher in the Rye', authors: "J. D. Salinger", imageLinks: {thumbnail: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2721385247,1356253863&fm=206&gp=0.jpg'}}}
+
 ];
 class BookList extends Component {
-  render() {
-    let book = FAKE_BOOK_DATA[0];
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged:(row1,row2) => row1 != row2,
+      })
+    };
+  }
+  componentDidMount() {
+    let books = FAKE_BOOK_DATA;
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(books),
+    });
+  }
+  _renderBook(book){
     return (
-      <View style = {styles.container}>
-        <Image source = {{uri:book.volumeInfo.imageLinks.thumbnail}}
-          style = {styles.thumbnail} />
-        <View style = {styles.rightContainer}>
-          <Text style = {styles.title}> { book.volumeInfo.title} </Text>
-          <Text style = {styles.author}> {book.volumeInfo.authors} </Text>
+      <TouchableHighlight>
+        <View>
+          <View style={styles.container}>
+            <Image
+              source={{uri: book.volumeInfo.imageLinks.thumbnail}}
+              style={styles.thumbnail} />
+            <View style={styles.rightContainer}>
+              <Text style={styles.title}>{book.volumeInfo.title}</Text>
+              <Text style={styles.author}>{book.volumeInfo.authors}</Text>
+            </View>
+          </View>
+          <View style={styles.separator} />
         </View>
-      </View>
+      </TouchableHighlight>
+    )
+  }
+  render() {
+    return (
+      <ListView
+        dataSource = {this.state.dataSource}
+        renderRow = {this._renderBook.bind(this)}
+        style = {styles.listView}>
+      </ListView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent:'center',
-    alignItems:'center',
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#F5FCFF',
     padding: 10
   },
-  thumbnail:{
-    width:53,
-    height:81,
-    marginRight:10,
+  thumbnail: {
+    width: 53,
+    height: 81,
+    marginRight: 10
   },
-  rightContainer:{
-    flex:1,
-    backgroundColor: 'red',
+  rightContainer: {
+    flex: 1
   },
-  title:{
-    fontSize:20,
-    // backgroundColor:'yellow',
+  title: {
+    fontSize: 20,
+    marginBottom: 8
   },
-  author:{
-    color:'#656565',
+  author: {
+    color: '#656565'
   },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  listView: {
+    backgroundColor: '#F5FCFF'
+  },
+  loading: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
 
 export default BookList;

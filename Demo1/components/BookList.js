2a1ef53624e85d70070df1c8fea91485b0f1efc4
xiaolei <coder_xiaolei@163.com>
2016-07-25 00:04:39 +0800

    ListView数据 OK

--- Demo1/components/BookList.js
+++ Demo1/components/BookList.js
@@ -9,39 +9,53 @@ import {
   Image,
   ListView,
   TouchableHighlight,
+  ActivityIndicator,
 } from 'react-native';
 
 let FAKE_BOOK_DATA = [
     {volumeInfo: {title: 'The Catcher in the Rye', authors: "J. D. Salinger", imageLinks: {thumbnail: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2721385247,1356253863&fm=206&gp=0.jpg'}}},
     {volumeInfo: {title: 'The Catcher in the Rye', authors: "J. D. Salinger", imageLinks: {thumbnail: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2721385247,1356253863&fm=206&gp=0.jpg'}}}
-
 ];
+let REQUEST_URL = 'https://api.douban.com/v2/book/search?count=100&q=react-native';
 class BookList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading: true,
       dataSource: new ListView.DataSource({
         rowHasChanged:(row1,row2) => row1 != row2,
       })
     };
   }
   componentDidMount() {
-    let books = FAKE_BOOK_DATA;
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(books),
-    });
+    this._fetchData();
+  }
+  _fetchData(){
+    fetch(REQUEST_URL)
+    .then((response) => response.json())
+    .then((responseData) =>{
+      console.log(responseData);
+      this.setState({
+        isLoading:false,
+        dataSource:this.state.dataSource.cloneWithRows(responseData.books),
+      })
+    })
+    .done();
+  }
+  _showBookDetail(book){
+    alert('图书详情');
   }
   _renderBook(book){
     return (
-      <TouchableHighlight>
+      <TouchableHighlight onPress={() => this._showBookDetail(book)} underlayColor='#dddddd'>
         <View>
           <View style={styles.container}>
             <Image
-              source={{uri: book.volumeInfo.imageLinks.thumbnail}}
+              source={{uri: book.image}}
               style={styles.thumbnail} />
             <View style={styles.rightContainer}>
-              <Text style={styles.title}>{book.volumeInfo.title}</Text>
-              <Text style={styles.author}>{book.volumeInfo.authors}</Text>
+              <Text style={styles.title}>{book.title}</Text>
+              <Text style={styles.author}>{book.author[0]}</Text>
             </View>
           </View>
           <View style={styles.separator} />
@@ -50,6 +64,16 @@ class BookList extends Component {
     )
   }
   render() {
+    if (this.state.isLoading) {
+      return(
+        <View style = {styles.isLoading}>
+          <ActivityIndicator size = 'large'/>
+          <Text style = {{textAlign:'center'}}>
+            Loading books...
+          </Text>
+        </View>
+      );
+    }
     return (
       <ListView
         dataSource = {this.state.dataSource}
@@ -67,7 +91,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-    padding: 10
+    padding: 10,
   },
   thumbnail: {
     width: 53,
@@ -89,7 +113,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#dddddd'
   },
   listView: {
-    backgroundColor: '#F5FCFF'
+    backgroundColor:'#F5FCFF',
+    marginTop:64,
+    marginBottom:48,
   },
   loading: {
     flex: 1,
